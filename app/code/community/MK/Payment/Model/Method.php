159744895c3181f0d332b03645e143e3ab352a10
Michael <mishkulik@gmail.com>
2017-05-18 19:30:00 +0700

    Completed Payment Method with secret code

--- app/code/community/MK/Payment/Model/Method.php
+++ app/code/community/MK/Payment/Model/Method.php
@@ -10,7 +10,10 @@ class MK_Payment_Model_Method extends Mage_Payment_Model_Method_Abstract
     public function validate()
     {
         // проверка секретного кода
-
+        $code = Mage::app()->getRequest()->getParam('secret_code');
+        if ($code != $this->getConfigData('secret_code')) { // getConfigData('secret_code') берет данные из system.xml узла <secret_code>
+            Mage::throwException(Mage::helper('mkpayment')->__('This code doesn\'t work!'));
+        }
         return parent::validate();
     }
 
